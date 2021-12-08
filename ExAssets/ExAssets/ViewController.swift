209bcd1fab3ef73322b191a60e32e9c948e0a9ed
Kim Jong Kwon <43035817+JK0369@users.noreply.github.com>
2021-12-09 01:38:33 +0900

    Add files via upload

---
+++ ExAssets/ExAssets/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ExAssets
+//
+//  Created by Jake.K on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let myImageAssets = MyAssetsCustomName.myPhoto
+    let image = myImageAssets.image
+  }
+}
+
